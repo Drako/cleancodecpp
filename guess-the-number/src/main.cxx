aee904cc5b79adf4d986c7ada4ca1e0be538cf92
Felix Bytow <drako@drako.guru>
2021-07-15 08:51:09 +0200

    :sparkles: added more exercises

--- guess-the-number/src/main.cxx
+++ guess-the-number/src/main.cxx
@@ -1,9 +1,13 @@
 #include "GuessTheNumber.hxx"
 #include "StdRandomNumberGenerator.hxx"
+#include "ConsoleUserInterface.hxx"
+
+#include <iostream>
 
 int main() {
 	StdRandomNumberGenerator rng;
-	GuessTheNumber game{rng};
+	ConsoleUserInterface ui{std::cout, std::cin};
+	GuessTheNumber game{rng, ui};
 	game.run();
 	return 0;
 }
---
+++ roman-numeral-converter/roman_numeral_converter.cxx
@@ -0,0 +1,11 @@
+#include "roman_numeral_converter.hxx"
+
+std::string workshop::to_roman(int value)
+{
+	return {};
+}
+
+int workshop::from_roman(std::string_view const value)
+{
+	return 0;
+}
